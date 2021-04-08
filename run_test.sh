534b4ec764c8a504cd47a92440cbd17adecf6a13
uvidit <uvidit@gamil.com>
2021-04-08 19:07:50 +0300

    added simple tests and with allure reporting

---
+++ run_test.sh
@@ -0,0 +1,11 @@
+export allure_results_dir=./allure_results
+export allure_report_dir=./allure_report
+
+rm -rf $allure_results_dir
+rm -rf allure_report_dir
+
+pytest --alluredir=$allure_results_dir
+#
+#
+#allure serve $allure_results_dir
+allure generate --clean --report-dir=$allure_report_dir $allure_results_dir
