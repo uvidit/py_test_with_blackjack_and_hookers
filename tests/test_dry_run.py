3bec2ea58fab2cb613882c7a1ba32073623816fa
uvidit <uvidit@gamil.com>
2021-04-09 18:02:47 +0300

    added url validating status code

--- tests/test_dry_run.py
+++ tests/test_dry_run.py
@@ -1,9 +1,17 @@
 import pytest
+import requests
+
 
 class TestClass:
     def test_success(self):
         """this test succeeds"""
         assert True
+
+    def test_url_success(self):
+        """this test validate URL response status"""
+        expectedStatusCode = 200
+        resp = requests.get("https://google.com")
+        assert resp.status_code == expectedStatusCode, f"Actual HTTP status code: {resp.status_code} , expected {expectedStatusCode = }"
 
     def test_success_one_more(self):
         """this test succeeds....there should be more 'green' test %) """
